611cf0f60e46bc7ad06dff20408e51a6a38f38fc
Atmayanti <mayablue74@gmail.com>
2022-06-05 23:54:45 +0700

    fix index

--- resources/views/auth/login.php
+++ resources/views/auth/login.php
@@ -1,53 +1,115 @@
-@extends('layouts.authLayout')
-@section('container')
-<form class="login100-form validate-form" style="background-color: #0c0c0c;">
-    <span class="login100-form-title p-b-43">
-        Masuk untuk Melanjutkan
-    </span>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Login V18</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="../../assets/images/icons/favicon.ico" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../assets/vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../assets/vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../assets/vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../assets/vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../assets/vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../assets/vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../assets/css/util.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/css/main2.css">
+    <!--===============================================================================================-->
+</head>
+
+<body>
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <form class="login100-form validate-form" style="background-color: #0c0c0c;">
+                    <span class="login100-form-title p-b-43">
+                        Masuk untuk Melanjutkan
+                    </span>
 
 
-    <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
-        <input class="input100" type="text" name="email">
-        <span class="focus-input100"></span>
-        <span class="label-input100">Email</span>
-    </div>
+                    <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+                        <input class="input100" type="text" name="email">
+                        <span class="focus-input100"></span>
+                        <span class="label-input100">Email</span>
+                    </div>
 
 
-    <div class="wrap-input100 validate-input" data-validate="Password is required">
-        <input class="input100" type="password" name="pass">
-        <span class="focus-input100"></span>
-        <span class="label-input100">Password</span>
-    </div>
+                    <div class="wrap-input100 validate-input" data-validate="Password is required">
+                        <input class="input100" type="password" name="pass">
+                        <span class="focus-input100"></span>
+                        <span class="label-input100">Password</span>
+                    </div>
 
-    <div class="flex-sb-m w-full p-t-3 p-b-32">
-        <div class="contact100-form-checkbox">
-            <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-            <label class="label-checkbox100" for="ckb1">
-                Ingat saya
-            </label>
+                    <div class="flex-sb-m w-full p-t-3 p-b-32">
+                        <div class="contact100-form-checkbox">
+                            <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+                            <label class="label-checkbox100" for="ckb1">
+                                Ingat saya
+                            </label>
+                        </div>
+
+                        <div>
+                            <a href="#" class="txt1">
+                                Lupa Password?
+                            </a>
+                        </div>
+                    </div>
+
+
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn btn-1">
+                            Masuk
+                        </button>
+                    </div>
+
+                    <div class="text-center p-t-46 p-b-20">
+                        <span class="txt2">
+                            Belum punya akun?
+                        </span>
+                        <span>
+                            <a href="/register" class="txt1">Daftar Sekarang</a>
+                        </span>
+                    </div>
+                </form>
+                <div class="login100-more" style="background-image: url('../../assets/images/bg-01.jpg');">
+                </div>
+            </div>
         </div>
-
-        <div>
-            <a href="#" class="txt1">
-                Lupa Password?
-            </a>
-        </div>
     </div>
 
 
-    <div class="container-login100-form-btn">
-        <button class="login100-form-btn btn-1">
-            Masuk
-        </button>
-    </div>
 
-    <div class="text-center p-t-46 p-b-20">
-        <span class="txt2">
-            Belum punya akun?
-        </span>
-        <span>
-            <a href="/register" class="txt1">Daftar Sekarang</a>
-        </span>
-    </div>
-</form>
-@endsection
+
+
+    <!--===============================================================================================-->
+    <script src="../../assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="../../assets/vendor/animsition/js/animsition.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="../../assets/vendor/bootstrap/js/popper.js"></script>
+    <script src="../../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="../../assets/vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="../../assets/vendor/daterangepicker/moment.min.js"></script>
+    <script src="../../assets/vendor/daterangepicker/daterangepicker.js"></script>
+    <!--===============================================================================================-->
+    <script src="../../assets/vendor/countdowntime/countdowntime.js"></script>
+    <!--===============================================================================================-->
+    <script src="../../assets/js/main2.js"></script>
+
+</body>
+
+</html>
