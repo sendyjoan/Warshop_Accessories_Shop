b6d77145ddc03e1bdeff13a2b147971e72ce511a
Sendy Joan K <sendyjoan5@gmail.com>
2022-06-06 22:56:49 +0700

    Add Read, Read Detail, Delete Product

--- product.php
+++ product.php
@@ -1,5 +1,9 @@
 <?php
-
+// Create database connection using config file
+include_once("config.php");
+ 
+// Fetch all users data from database
+$result = mysqli_query($mysqli, "SELECT * FROM products ORDER BY idproduct ASC");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,5 +15,23 @@
 </head>
 <body>
     <h1>Product Page</h1>
+
+    <table width='80%' border=1>
+ 
+    <tr>
+        <th>Name</th> <th>Summary</th> <th>Description</th> <th>Price</th> <th>Stock</th> <th>Action</th>
+    </tr>
+    <?php  
+    while($user_data = mysqli_fetch_array($result)) {         
+        echo "<tr>";
+        echo "<td>".$user_data['namabarang']."</td>";
+        echo "<td>".$user_data['ringkasan']."</td>";
+        echo "<td>".$user_data['deskripsi']."</td>";
+        echo "<td>".$user_data['harga']."</td>";
+        echo "<td>".$user_data['stock']."</td>";
+        echo "<td><a href='detail_product.php?id=$user_data[idproduct]'>Detail</a>";
+    }
+    ?>
+    </table>
 </body>
 </html>
