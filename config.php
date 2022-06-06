de3953480389ac47c30396f6ce2ac8d8702351ce
Sendy Joan K <sendyjoan5@gmail.com>
2022-06-06 21:43:22 +0700

    Update config.php
    Menambahkan Debuger Pada config.php

--- config.php
+++ config.php
@@ -9,5 +9,11 @@ $databaseUsername = 'root';
 $databasePassword = '';
  
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+if (!$mysqli) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+echo "Koneksi berhasil";
+mysqli_close($mysqli);
  
 ?>
