546b232f68d4056e8646bfb36ddd4941631b6d2a
Sendy Joan K <sendyjoan5@gmail.com>
2022-06-09 10:30:42 +0700

    Add Feature Show User

--- showuser.php
+++ showuser.php
@@ -1,3 +1,8 @@
+<?php
+    include_once("config.php");
+
+    $result = mysqli_query($mysqli, "SELECT * FROM users INNER JOIN role ON users.role_id = role.idrole INNER JOIN genders ON users.gender_id = genders.idjeniskelamin;");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +13,37 @@
 </head>
 <body>
     <h1>Show User</h1>
-    
+    <table width='80%' border=1>
+
+        <tr>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jenis Kelamin</th>
+            <th>Address</th>
+            <th>Telephone</th>
+            <th>Tanggal Lahir</th>
+            <th>Tempat Lahir</th>
+            <th>Picture</th>
+            <th>Role</th>
+            <th>Action</th>
+        </tr>
+        <?php  
+        while($data = mysqli_fetch_array($result)) {         
+            echo "<tr>";
+            echo "<td>".$data['name']."</td>";
+            echo "<td>".$data['email']."</td>";
+            echo "<td>".$data['jeniskelamin']."</td>";
+            echo "<td>".$data['address']."</td>";
+            echo "<td>".$data['telephone']."</td>";
+            echo "<td>".$data['tanggallahir']."</td>";
+            echo "<td>".$data['tempatlahir']."</td>";
+            echo "<td>".$data['picture']."</td>";
+            echo "<td>".$data['role']."</td>";
+            echo "<td><a href='detail_product.php?id=$data[id]'>Detail</a> <br>
+            <a href='edit_product.php?id=$data[id]'>Edit</a> <br> 
+            <a href='delete_product.php?id=$data[id]'>Delete</a> </td>";
+        }
+        ?>
+    </table>
 </body>
 </html>
